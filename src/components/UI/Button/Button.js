3436e46c003ccb54a8236f93d34839552debb453
swapnilbhakare <swapnilbhakare7@gmail.com>
2023-06-13 13:51:41 +0530

    Add goal button chnaged dynamically

--- src/components/UI/Button/Button.js
+++ src/components/UI/Button/Button.js
@@ -1,10 +1,14 @@
-import React from 'react';
+import React from "react";
 
-import './Button.css';
+import "./Button.css";
 
-const Button = props => {
+const Button = (props) => {
   return (
-    <button type={props.type} className="button" onClick={props.onClick}>
+    <button
+      type={props.type}
+      className={`button ${!props.invalid ? "invlid" : ""}`}
+      onClick={props.onClick}
+    >
       {props.children}
     </button>
   );
